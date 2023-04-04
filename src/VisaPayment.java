48f8572b5e2fb20a5bbe7a34881fe98f2ac05a0e
AdiletKushubekov <adiletkushubekov@gmail.com>
2023-04-04 23:03:12 +0600

    first-commit

---
+++ src/VisaPayment.java
@@ -0,0 +1,9 @@
+public class VisaPayment {
+    public static void main(String[] args) {
+        System.out.println(visaPay());
+
+    }
+    public static String visaPay(){
+        return "Оплата картой виза";
+    }
+}
